e3da99544c9665a627fae2e904c78268e680f2b5
LufDevOps <devops.hoanghagiang@gmail.com>
2024-04-15 11:05:26 +0700

    deploy new server

--- old_build/migrateup.sh
+++ old_build/migrateup.sh
@@ -2,6 +2,7 @@
 
 echo "Postgres is up - begin migrating"
 # Run migration up
-#docker exec -it postgres createdb --username=root --owner=root simple_bank
+
+docker exec -it postgres createdb --username=postgres --owner=postgres postgres
 
 migrate -path ./migration -database "postgresql://$POSTGRESQL_USER:$POSTGRESQL_PASSWORD@$POSTGRESQL_HOST:$POSTGRESQL_PORT/$POSTGRESQL_DB?sslmode=disable" -verbose up
