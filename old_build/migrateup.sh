9d449a8ae55c58afd5972e732557f4e9d12ae26e
LufDevOps <devops.hoanghagiang@gmail.com>
2024-04-15 13:46:40 +0700

    first commit

--- old_build/migrateup.sh
+++ old_build/migrateup.sh
@@ -3,6 +3,6 @@
 echo "Postgres is up - begin migrating"
 # Run migration up
 
-docker exec -it postgres createdb --username=postgres --owner=postgres postgres
+#docker exec -it postgres createdb --username=postgres --owner=postgres postgres
 
 migrate -path ./migration -database "postgresql://$POSTGRESQL_USER:$POSTGRESQL_PASSWORD@$POSTGRESQL_HOST:$POSTGRESQL_PORT/$POSTGRESQL_DB?sslmode=disable" -verbose up
